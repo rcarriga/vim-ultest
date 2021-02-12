dd5598a819894dc19b917c2f5f244e7e7f7a7993
Rónán Carrigan <rcarriga@tcd.ie>
2021-02-12 15:35:42 +0000

    feat: separate and store processes stdid/stdout

--- rplugin/python3/ultest/handler/runner.py
+++ rplugin/python3/ultest/handler/runner.py
@@ -1,20 +1,94 @@
+import logging
+import os
 import tempfile
 from asyncio import subprocess
-from typing import Iterable, List
+from contextlib import contextmanager
+from io import BufferedReader, BufferedWriter
+from os import path
+from threading import Event, Thread
+from typing import Dict, Iterable, Iterator, List, Tuple
 
 from ..models import Result, Test
 from ..vim import VimClient
 
 
-class Runner:
+class TestProcess:
+    def __init__(self, in_path: str, out_path: str):
+        self._in_path = in_path
+        self._out_path = out_path
+        self._close_event = Event()
+        self._create_stdin()
+        self._wipe_stdout()
+
+    def _create_stdin(self):
+        if os.path.exists(self._in_path):
+            os.remove(self._in_path)
+        os.mkfifo(self._in_path, 0o777)
+
+    def _wipe_stdout(self):
+        if os.path.exists(self._out_path):
+            os.remove(self._out_path)
+
+    def _keep_stdin_open(self):
+        def keep_open():
+            with open(self._in_path, "wb"):
+                self._close_event.wait()
+                self._close_event.clear()
+
+        Thread(target=keep_open).start()
+
+    @contextmanager
+    def open(self) -> Iterator[Tuple[BufferedReader, BufferedWriter]]:
+        in_handle = self._open_stdin()
+        out_handle = self._open_stdout()
+        try:
+            yield (in_handle, out_handle)
+        finally:
+            self._close_stdin(in_handle)
+            self._close_stdout(out_handle)
+
+    def _open_stdin(self) -> BufferedReader:
+        if self._close_event.is_set():
+            raise IOError(f"Handle is already open for {self._in_path}")
+        self._keep_stdin_open()
+        return open(self._in_path, "rb")
+
+    def _close_stdin(self, reader: BufferedReader):
+        self._close_event.set()
+        reader.close()
+
+    def _close_stdout(self, writer: BufferedWriter):
+        writer.close()
+
+    def _open_stdout(self) -> BufferedWriter:
+        return open(self._out_path, "wb")
+
+
+class ProcessManager:
     """
     Handle scheduling and running tests.
     """
 
     def __init__(self, vim: VimClient):
         self._vim = vim
+        self._dir = tempfile.TemporaryDirectory(prefix="ultest")
+        self._processes: Dict[str, TestProcess] = {}
 
-    async def execute_test(self, cmd: List[str], test: Test):
+    def _test_file_dir(self, file: str) -> str:
+        return os.path.join(self._dir.name, file.replace(os.path.pathsep, "__"))
+
+    def _create_test_file_dir(self, file: str):
+        path = self._test_file_dir(file)
+        if not os.path.isdir(path):
+            os.mkdir(path)
+
+    def stdin_name(self, test: Test) -> str:
+        return path.join(self._test_file_dir(test.file), f"{test.id}_in")
+
+    def stdout_name(self, test: Test) -> str:
+        return path.join(self._test_file_dir(test.file), f"{test.id}_out")
+
+    async def run(self, cmd: List[str], test: Test):
         """
         Run a test with the given command.
 
@@ -26,19 +100,20 @@ class Runner:
         :rtype: Result
         """
 
-        (output_handle, output_path) = tempfile.mkstemp()
-        with open(output_handle, "wb+") as handle:
-            completed = subprocess.create_subprocess_exec(
-                *cmd, stdin=handle, stderr=handle, stdout=handle
+        self._create_test_file_dir(test.file)
+        stdin_path = self.stdin_name(test)
+        stdout_path = self.stdout_name(test)
+        test_process = TestProcess(in_path=stdin_path, out_path=stdout_path)
+        self._processes[test.id] = test_process
+        with test_process.open() as (in_handle, out_handle):
+            logging.error(f"{in_handle} {out_handle}")
+            process = await subprocess.create_subprocess_exec(
+                *cmd, stdin=in_handle, stderr=out_handle, stdout=out_handle
             )
-            process = await completed
             code = await process.wait()
-            return Result(
-                id=test.id,
-                file=test.file,
-                code=code,
-                output=output_path,
-            )
+            result = Result(id=test.id, file=test.file, code=code, output=stdout_path)
+            del self._processes[test.id]
+            return result
 
     async def run_tests(self, tests: Iterable[Test]):
         """
@@ -49,3 +124,6 @@ class Runner:
             test.running = 1
             self._vim.call("ultest#process#start", test)
             self._vim.call("ultest#adapter#run_test", test)
+
+    def clear(self):
+        self._dir.cleanup()
