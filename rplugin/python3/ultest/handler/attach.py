567120dfa2a6dfc0c3de178dc86e5352028141a9
Rónán Carrigan <rcarriga@tcd.ie>
2021-03-03 09:16:25 +0000

    fix(attach): use prompt for readline

--- rplugin/python3/ultest/handler/attach.py
+++ rplugin/python3/ultest/handler/attach.py
@@ -1,25 +1,28 @@
 import readline  # type: ignore
 import subprocess
-import sys
 
 IN_FILE = "{test_process.in_path}"
 OUT_FILE = "{test_process.out_path}"
 
+# Use a non visible prompt to prevent readline overwriting text from stdout
+# on the last line, where stdin is also written. This is still not perfect,
+# for example vi bindings still allow deleting prompt text.
+PROMPT = "\010"
+
 
 def run():
-    devnull = open("/dev/null", "a")
     to_input = open(IN_FILE, "wb")
 
     subprocess.Popen(
         ["tail", "-F", "-c", "+0", OUT_FILE],
-        stdin=devnull,
-        stdout=sys.stdout,
+        stdin=subprocess.DEVNULL,
+        stdout=None,
         stderr=subprocess.STDOUT,
     )
 
     try:
         while True:
-            in_ = input() + "\n"
+            in_ = input(PROMPT) + "\n"
             to_input.write(in_.encode())
             to_input.flush()
     except BaseException:
