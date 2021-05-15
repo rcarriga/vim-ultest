019be537b048adb441b781d5a2f16088cd7b979e
Rónán Carrigan <rcarriga@tcd.ie>
2021-05-15 13:41:18 +0100

    test: disable too_slow healthcheck

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,8 +7,11 @@ import ultest  # type: ignore
 
 sys.modules["rplugin.python3.ultest"] = ultest
 
+import hypothesis
 from hypothesis import settings
 
-settings.register_profile("default", max_examples=1)
+settings.register_profile(
+    "default", max_examples=1, suppress_health_check=[hypothesis.HealthCheck.too_slow]
+)
 
 settings.load_profile("default")
