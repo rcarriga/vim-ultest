rcarriga/vim-ultest

commit 616bbca2e5d0a97edadb441df56bc21598e59a1a

fix: dummmy user autocmds
Vim requires User autocmds to exist before emitting them.

See #98

plugin/ultest.vim
@@ -427,6 +427,14 @@ augroup UltestPositionUpdater
   endif
 augroup END
 
+if !has("nvim")
+  augroup UltestDummyCommand
+    au!
+    au User UltestPositionsUpdate let <SID>dummy = 1
+    au User UltestOutputOpen let <SID>dummy = 1
+  augroup END
+endif
+
 if !has("vim_starting")
   " Avoids race condition https://github.com/neovim/pynvim/issues/341
   call ultest#handler#get_attach_script("")

commit 6ac9d14955b3506af3b7dda12f1b58e5addb6df4

Fixes issue introduced with 616bbca2e5d0a97edadb441df56bc21598e59a1a
Setting a dummy variable was failing sayung s:dummy was not found. A
dummy call works without any issues.

plugin/ultest.vim
@@ -459,11 +459,14 @@ augroup UltestPositionUpdater
   endif
 augroup END
 
+function! s:dummy()
+endfunction
+
 if !has("nvim")
   augroup UltestDummyCommand
     au!
-    au User UltestPositionsUpdate let <SID>dummy = 1
-    au User UltestOutputOpen let <SID>dummy = 1
+    au User UltestPositionsUpdate call s:dummy()
+    au User UltestOutputOpen call s:dummy()
   augroup END
 endif
 